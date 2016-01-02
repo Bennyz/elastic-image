34804598a23275be4df4485529d4bb94c0f50f32
Benny Zlotnik <uber442@gmail.com>
2016-01-02 21:07:45 +0200

    Add stuff

--- Dockerfile
+++ Dockerfile
@@ -10,10 +10,11 @@ ADD config/elasticsearch.yml $ELASTIC_HOME/config/
 ADD config/logging.yml $ELASTIC_HOME/config/
 RUN chown elasticsearch -R $ELASTIC_HOME
 CMD ["sysctl","-w","vm.max_map_count=262144"]
-CMD ["ulimit","-l","ulimited"]
+CMD ["ulimit","-l","unlimited"]
+COPY limits.conf /etc/security/
 
 USER elasticsearch
-ENV ES_HEAP_SIZE 256m
+ENV ES_HEAP_SIZE 128m
 ENTRYPOINT ["elasticsearch"]
 
 EXPOSE 9200 9300
--- start_cluster.sh
+++ start_cluster.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-docker run -m=1g -d -p 10.0.0.30:9200:9200 -p 10.0.0.30:9300:9300 --hostname=esmaster1 -v /etc/hosts:/etc/hosts bigdata/elastic --network.publish_host=esmaster1 \
+docker run -d -p 10.0.0.99:80:9000 -p 10.0.0.99:8001:8001 --hostname=elastic es/haproxy
+
+docker run -m=1g --cap-add=IPC_LOCK --ulimit memlock=-1:-1 -d -p 10.0.0.30:9200:9200 -p 10.0.0.30:9300:9300 --hostname=esmaster1 -v /etc/hosts:/etc/hosts bigdata/elastic --network.publish_host=esmaster1 \
     --network.bind_host=0.0.0.0 \
     --discovery.zen.ping.multicast.enabled=false \
     --discovery.zen.ping.unicast.hosts=esmaster1,esmaster2 \
@@ -10,8 +12,8 @@ docker run -m=1g -d -p 10.0.0.30:9200:9200 -p 10.0.0.30:9300:9300 --hostname=esm
     --http.enabled=false \
     --node.master=true \
     --node.data=false
-: '
-docker run -m=1g -d -p 10.0.0.40:9200:9200 -p 10.0.0.40:9300:9300 --hostname=estmaster2 -v /etc/hosts:/etc/hosts bigdata/elastic --network.publish_host=esmaster2 \
+
+docker run -m=1g -d --cap-add=IPC_LOCK --ulimit memlock=-1:-1 -p 10.0.0.40:9200:9200 -p 10.0.0.40:9300:9300 --hostname=estmaster2 -v /etc/hosts:/etc/hosts bigdata/elastic --network.publish_host=esmaster2 \
     --network.bind_host=0.0.0.0 \
     --discovery.zen.ping.multicast.enabled=false \
     --discovery.zen.ping.unicast.hosts=esmaster1,esmaster2 \
@@ -21,10 +23,11 @@ docker run -m=1g -d -p 10.0.0.40:9200:9200 -p 10.0.0.40:9300:9300 --hostname=est
     --http.enabled=false \
     --node.master=true \
     --node.data=false \
-'
+    --bootstrap.mlockall=true
 
 
-docker run -m=1g -d -p 10.0.0.10:9200:9200 -p 10.0.0.10:9300:9300 --hostname=esclient1 -v /etc/hosts:/etc/hosts bigdata/elastic --network.publish_host=esclient1 \
+
+docker run -m=1g -d --cap-add=IPC_LOCK --ulimit memlock=-1:-1 -p 10.0.0.10:9200:9200 -p 10.0.0.10:9300:9300 --hostname=esclient1 -v /etc/hosts:/etc/hosts bigdata/elastic --network.publish_host=esclient1 \
     --network.bind_host=0.0.0.0 \
     --discovery.zen.ping.multicast.enabled=false \
     --discovery.zen.ping.unicast.hosts=esmaster1,esmaster2 \
@@ -34,8 +37,9 @@ docker run -m=1g -d -p 10.0.0.10:9200:9200 -p 10.0.0.10:9300:9300 --hostname=esc
     --cluster.name=docker_cluster \
     --node.master=false \
     --node.data=false \
+    --bootstrap.mlockall=true
 
-docker run -m=1g -d -p 10.0.0.20:9200:9200 -p 10.0.0.20:9300:9300 --hostname=esclient2 -v /etc/hosts:/etc/hosts bigdata/elastic --network.publish_host=esclient2 \
+docker run -m=1g -d --cap-add=IPC_LOCK --ulimit memlock=-1:-1 -p 10.0.0.20:9200:9200 -p 10.0.0.20:9300:9300 --hostname=esclient2 -v /etc/hosts:/etc/hosts bigdata/elastic --network.publish_host=esclient2 \
     --network.bind_host=0.0.0.0 \
     --discovery.zen.ping.multicast.enabled=false \
     --discovery.zen.ping.unicast.hosts=esmaster1,esmaster2 \
@@ -45,8 +49,9 @@ docker run -m=1g -d -p 10.0.0.20:9200:9200 -p 10.0.0.20:9300:9300 --hostname=esc
     --cluster.name=docker_cluster \
     --node.master=false \
     --node.data=false \
+    --bootstrap.mlockall=true
 
-docker run -m=4g -d -p 10.0.0.50:9200:9200 -p 10.0.0.50:9300:9300 --hostname=esdata1 -v /etc/hosts:/etc/hosts bigdata/elastic --network.publish_host=esdata1 \
+docker run -m=2g -d --cap-add=IPC_LOCK --ulimit memlock=-1:-1 -p 10.0.0.50:9200:9200 -p 10.0.0.50:9300:9300 --hostname=esdata1 -v /etc/hosts:/etc/hosts bigdata/elastic --network.publish_host=esdata1 \
     --network.bind_host=0.0.0.0 \
     --discovery.zen.ping.multicast.enabled=false \
     --discovery.zen.ping.unicast.hosts=esmaster1,esmaster2 \
@@ -55,20 +60,22 @@ docker run -m=4g -d -p 10.0.0.50:9200:9200 -p 10.0.0.50:9300:9300 --hostname=esd
     --cluster.name=docker_cluster \
     --http.enabled=false \
     --node.master=false \
-    --node.data=true 
+    --node.data=true \
+    --bootstrap.mlockall=true
+
+docker run -m=2g -d --cap-add=IPC_LOCK --ulimit memlock=-1:-1 -p 10.0.0.60:9200:9200 -p 10.0.0.60:9300:9300 --hostname=esdata2 -v /etc/hosts:/etc/hosts bigdata/elastic --network.publish_host=esdata2 \
+    --network.bind_host=0.0.0.0 \
+    --discovery.zen.ping.multicast.enabled=false \
+    --discovery.zen.ping.unicast.hosts=esmaster1,esmaster2 \
+    --discovery.zen.ping.timeout=5s \
+    --discovery.zen.minimum_master_nodes=1 \
+    --cluster.name=docker_cluster \
+    --node.master=false \
+    --http.enabled=false \
+    --node.data=true \
+    --bootstrap.mlockall=true
 
 : '
-docker run -m=4g -d -p 10.0.0.60:9200:9200 -p 10.0.0.60:9300:9300 --hostname=esdata2 -v /etc/hosts:/etc/hosts bigdata/elastic --network.publish_host=esdata2 \
-    --network.bind_host=0.0.0.0 \
-    --discovery.zen.ping.multicast.enabled=false \
-    --discovery.zen.ping.unicast.hosts=esmaster1,esmaster2 \
-    --discovery.zen.ping.timeout=5s \
-    --discovery.zen.minimum_master_nodes=1 \
-    --cluster.name=docker_cluster \
-    --node.master=false \
-    --http.enabled=false \
-    --node.data=true 
-
 docker run -d -p 10.0.0.70:9200:9200 -p 10.0.0.70:9300:9300 --hostname=esdata3 -v /etc/hosts:/etc/hosts bigdata/elastic --network.publish_host=esdata3 \
     --network.bind_host=0.0.0.0 \
     --discovery.zen.ping.multicast.enabled=false \
